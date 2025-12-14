e99152c28692e91cc4a0d0c0e5c778ea561a0674
wanderleyjunior000 <wanderley.junior@skf.com>
2025-12-14 12:46:33 -0300

    Configuração para deploy no Render (Postgres + Gunicorn)

--- bella_designer/settings.py
+++ bella_designer/settings.py
@@ -4,7 +4,6 @@ import os
 from pathlib import Path
 import os
 from dotenv import load_dotenv
-import dj_database_url
 
 # Chame isso em algum lugar perto do BASE_DIR se ainda não estiver fazendo
 load_dotenv()
